75cccd5c2a24350461b24a21b001c82b90ca0810
Henry.Huang <hhglory@outlook.com>
2022-07-25 22:45:42 +0800

    solutions: ex054, pointer, hard

--- exercises/054_manypointers.zig
+++ exercises/054_manypointers.zig
@@ -1,9 +1,9 @@
 //
 // You can also make pointers to multiple items without using a slice.
 //
-//     var foo: [4]u8 = [4]u8{ 1, 2, 3, 4 };
+//     var foo: [4]u8 = [4]u8{ 1, 2, 3, 4 }; // todo x: 定长数组
 //     var foo_slice: []u8 = foo[0..];
-//     var foo_ptr: [*]u8 = &foo;
+//     var foo_ptr: [*]u8 = &foo; // todo x: 通过指针, 访问字符串元素
 //
 // The difference between foo_slice and foo_ptr is that the slice has
 // a known length. The pointer doesn't. It is up to YOU to keep track
@@ -15,13 +15,15 @@ pub fn main() void {
     // Take a good look at the array type to which we're coercing
     // the zen12 string (the REAL nature of strings will be
     // revealed when we've learned some additional features):
-    const zen12: *const [21]u8 = "Memory is a resource.";
+    const zen12: *const [21]u8 = "Memory is a resource."; // todo x:
     //
     //   It would also have been valid to coerce to a slice:
     //         const zen12: []const u8 = "...";
     //
     // Now let's turn this into a "many-item pointer":
-    const zen_manyptr: [*]const u8 = zen12;
+    const zen_manyptr: [*]const u8 = zen12; // todo x: 
+
+    // std.debug.print("zen_manyptr: {s},", .{zen_manyptr});
 
     // It's okay to access zen_manyptr just like an array or slice as
     // long as you keep track of the length yourself!
@@ -32,7 +34,7 @@ pub fn main() void {
     // we can CONVERT IT TO A SLICE. (Hint: we do know the length!)
     //
     // Please fix this line so the print statement below can print it:
-    const zen12_string: []const u8 = zen_manyptr;
+    const zen12_string: []const u8 = zen_manyptr[0..21]; // todo x: ??? 不好理解
 
     // Here's the moment of truth!
     std.debug.print("{s}\n", .{zen12_string});
@@ -43,12 +45,12 @@ pub fn main() void {
 //     FREE ZIG POINTER CHEATSHEET! (Using u8 as the example type.)
 //   +---------------+----------------------------------------------+
 //   |  u8           |  one u8                                      |
-//   |  *u8          |  pointer to one u8                           |
+//   |  *u8          |  pointer to one u8                           | // todo x:指针, 指向单一元素
 //   |  [2]u8        |  two u8s                                     |
-//   |  [*]u8        |  pointer to unknown number of u8s            |
-//   |  [*]const u8  |  pointer to unknown number of immutable u8s  |
-//   |  *[2]u8       |  pointer to an array of 2 u8s                |
-//   |  *const [2]u8 |  pointer to an immutable array of 2 u8s      |
+//   |  [*]u8        |  pointer to unknown number of u8s            | // todo x: 指针, 指向切片
+//   |  [*]const u8  |  pointer to unknown number of immutable u8s  | // todo x: 指针, 指向不可变切片 ??? // 不好理解
+//   |  *[2]u8       |  pointer to an array of 2 u8s                | // todo x: 指针, 指向定长数组
+//   |  *const [2]u8 |  pointer to an immutable array of 2 u8s      | // todo x: 指针, 指向不可变定长数组
 //   |  []u8         |  slice of u8s                                |
-//   |  []const u8   |  slice of immutable u8s                      |
+//   |  []const u8   |  slice of immutable u8s                      | // todo x: 不可变切片
 //   +---------------+----------------------------------------------+
