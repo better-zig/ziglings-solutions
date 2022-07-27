18d8adc0260d97549331eccc0947129e77599c7f
Henry.Huang <hhglory@outlook.com>
2022-07-28 03:11:23 +0800

    solutions: ex087, async + suspend + resume + global var

--- exercises/087_async4.zig
+++ exercises/087_async4.zig
@@ -3,20 +3,22 @@
 // longer capturing a return value from foo() because the 'async'
 // keyword returns the frame instead.
 //
-// One way to solve this is to use a global variable.
+// One way to solve this is to use a global variable. // todo x: 使用全局变量
 //
 // See if you can make this program print "1 2 3 4 5".
 //
 const print = @import("std").debug.print;
 
-var global_counter: i32 = 0;
+var global_counter: i32 = 0; // todo x: 全局变量
 
 pub fn main() void {
-    var foo_frame = async foo();
+    var foo_frame = async foo(); // todo x: 异步方法
 
     while (global_counter <= 5) {
         print("{} ", .{global_counter});
-        ???
+
+        // todo x: 线程(协程)间 - 控制流切换
+        resume foo_frame; // todo x: 恢复到挂起处
     }
 
     print("\n", .{});
@@ -24,7 +26,7 @@ pub fn main() void {
 
 fn foo() void {
     while (true) {
-        ???
-        ???
+        global_counter += 1; // TODO x: 计数器
+        suspend {} // todo x: 挂起+返回
     }
 }
