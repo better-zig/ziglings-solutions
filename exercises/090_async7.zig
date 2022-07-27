a26cfd2a40886e33267932e0927dbbe05514f3ab
Henry.Huang <hhglory@outlook.com>
2022-07-28 03:32:45 +0800

    solutions: ex090, nosuspend + suspend, panic UB error

--- exercises/090_async7.zig
+++ exercises/090_async7.zig
@@ -1,25 +1,25 @@
 //
-// Remember how a function with 'suspend' is async and calling an
+// Remember how a function with 'suspend' is async and calling an // todo x: 存在 suspend, 显式异步
 // async function without the 'async' keyword makes the CALLING
 // function async?
 //
 //     fn fooThatMightSuspend(maybe: bool) void {
-//         if (maybe) suspend {}
+//         if (maybe) suspend {} // todo x: suspend 异步方法, 挂起, 提前返回
 //     }
 //
 //     fn bar() void {
-//         fooThatMightSuspend(true); // Now bar() is async!
+//         fooThatMightSuspend(true); // Now bar() is async! // todo x: 异步传染性!
 //     }
 //
 // But if you KNOW the function won't suspend, you can make a
 // promise to the compiler with the 'nosuspend' keyword:
 //
 //     fn bar() void {
-//         nosuspend fooThatMightSuspend(false);
+//         nosuspend fooThatMightSuspend(false); // todo x: nosuspend 关键字, 让编译期忽略
 //     }
 //
 // If the function does suspend and YOUR PROMISE TO THE COMPILER
-// IS BROKEN, the program will panic at runtime, which is
+// IS BROKEN, the program will panic at runtime, which is // todo x: 如果方法出现 suspend, 会在运行时 Panic
 // probably better than you deserve, you oathbreaker! >:-(
 //
 const print = @import("std").debug.print;
@@ -29,14 +29,19 @@ pub fn main() void {
     // The main() function can not be async. But we know
     // getBeef() will not suspend with this particular
     // invocation. Please make this okay:
-    var my_beef = getBeef(0);
-
+    var my_beef = nosuspend getBeef(0); // todo x: 显式忽略挂起信号
     print("beef? {X}!\n", .{my_beef});
+
+    //
+    // todo x: 下述代码, 会 Panic, 触发 UB(未定义行为)报错
+    //
+    // var my_beef2 = nosuspend getBeef(0xDEAD); // todo x: 显式忽略挂起信号
+    // print("beef? {X}!\n", .{my_beef2});
 }
 
 fn getBeef(input: u32) u32 {
     if (input == 0xDEAD) {
-        suspend {}
+        suspend {} // todo x: 线程挂起, 线程切换
     }
 
     return 0xBEEF;
@@ -45,7 +50,7 @@ fn getBeef(input: u32) u32 {
 // Going Deeper Into...
 //                     ...uNdeFiNEd beHAVi0r!
 //
-// We haven't discussed it yet, but runtime "safety" features
+// We haven't discussed it yet, but runtime "safety" features // todo x: 运行时安全机制
 // require some extra instructions in your compiled program.
 // Most of the time, you're going to want to keep these in.
 //
@@ -54,7 +59,7 @@ fn getBeef(input: u32) u32 {
 // without these safety features.
 //
 // Instead of a safe panic when something goes wrong, your
-// program will now exhibit Undefined Behavior (UB), which simply
+// program will now exhibit Undefined Behavior (UB), which simply // todo x: 未定义行为
 // means that the Zig language does not (cannot) define what will
 // happen. The best case is that it will crash, but in the worst
 // case, it will continue to run with the wrong results and
