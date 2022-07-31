dd887f8ca1d4fc6bf3e9b311b0a5a595662ae1c3
Henry.Huang <hhglory@outlook.com>
2022-07-31 21:08:51 +0800

    update: vscode debug

--- exercises/015_for.zig
+++ exercises/015_for.zig
@@ -10,12 +10,16 @@
 //
 const std = @import("std");
 
+const x = 2233;
+var y: f64 = 22.33;
+
 //
 // todo x: for 遍历写法
 //
 pub fn main() void {
     const story = [_]u8{ 'h', 'h', 's', 'n', 'h' };
 
+    std.debug.print("x = {}, y = {}\n", .{ x, y }); // todo x: 测试 debug 插件, 断点调试, 捕获全局变量/常量
     std.debug.print("A Dramatic Story: ", .{});
 
     for (story) |scene| { // todo x: for 遍历写法, 类似闭包
